70108dd927790fbedbb60d160ec9372253757ecb
Jeff Hudren <jeff@hudren.com>
2014-03-25 21:39:34 -0600

    Listing files

--- src/single_track/core.clj
+++ src/single_track/core.clj
@@ -1,6 +1,19 @@
-(ns single-track.core)
+(ns single-track.core
+  (:require [clojure.java.io :as io]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn file-filter [file]
+  (not= \. (-> file .getName first)))
+
+(defn my-file-seq
+  "A tree seq on java.io.Files"
+  ([dir]
+    (my-file-seq dir (fn [_] true)))
+  ([dir pred]
+    (tree-seq
+     (fn [^java.io.File f] (. f (isDirectory)))
+     (fn [^java.io.File d] (filter pred (seq (. d (listFiles)))))
+     dir)))
+
+(defn -main [dir]
+  (doseq [f (my-file-seq (io/file dir) file-filter)]
+    (println f)))
