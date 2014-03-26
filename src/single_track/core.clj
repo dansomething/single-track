dcb341a8be2d270d4afe04edae85040f5de985a8
Jeff Hudren <jeff@hudren.com>
2014-03-26 07:05:29 -0600

    code cleanup

--- src/single_track/core.clj
+++ src/single_track/core.clj
@@ -1,19 +1,22 @@
 (ns single-track.core
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:import [java.io File]))
 
-(defn file-filter [file]
+(set! *warn-on-reflection* true)
+
+(defn visible-files [^File file]
   (not= \. (-> file .getName first)))
 
-(defn my-file-seq
-  "A tree seq on java.io.Files"
-  ([dir]
-    (my-file-seq dir (fn [_] true)))
-  ([dir pred]
-    (tree-seq
-     (fn [^java.io.File f] (. f (isDirectory)))
-     (fn [^java.io.File d] (filter pred (seq (. d (listFiles)))))
-     dir)))
+(defn filtered-file-seq
+  "A filtered tree seq on java.io.Files"
+  [dir pred]
+  (tree-seq
+    (fn [^File f] (. f (isDirectory)))
+    (fn [^File d] (filter pred (. d (listFiles))))
+    dir))
 
-(defn -main [dir]
-  (doseq [f (my-file-seq (io/file dir) file-filter)]
-    (println f)))
+(defn -main
+  ([] (-main "."))
+  ([dir]
+   (doseq [f (filtered-file-seq (io/file dir) visible-files)]
+     (println (.getName f)))))
