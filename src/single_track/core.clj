8fa39915042b69e70c9da22f33aee2dc8a24ac1a
Jeff Hudren <jeff@hudren.com>
2014-04-04 06:28:57 -0600

    Code cleanup / refactoring

--- src/single_track/core.clj
+++ src/single_track/core.clj
@@ -1,39 +1,37 @@
 (ns single-track.core
   (:require [clojure.java.io :as io])
-  (:import [java.io File]))
+  (:import (java.io File)))
 
 (set! *warn-on-reflection* true)
-
-(def audio-ext #{"mp3" "wav"})
 
 (defn visible-files [^File file]
   (not= \. (-> file .getName first)))
 
 (defn filtered-file-seq
   "A filtered tree seq on java.io.Files"
-  [dir pred]
+  [pred dir]
   (tree-seq
     (fn [^File f] (. f (isDirectory)))
     (fn [^File d] (filter pred (. d (listFiles))))
     dir))
 
+(defn visible-file-seq [file]
+  (filtered-file-seq visible-files file))
+
 (defn file-ext [^File file]
   (let [name (.getName file)]
     (.substring name (inc (.lastIndexOf name ".")))))
 
-(defn audio? [^File file]
-  (audio-ext (file-ext file)))
+(def audio-ext #{"mp3" "wav"})
+(defn audio? [file] (audio-ext (file-ext file)))
+(defn file? [^File file] (not (.isDirectory file)))
 
-(defn file? [^File file]
-  (not (.isDirectory file)))
-
-(defn good? [file]
-  (and (file? file) (audio? file)))
+(defn all? [& fns] (fn [x] (every? #(% x) fns)))
 
 (defn -main
   ([] (-main "."))
   ([dir]
-   (doseq [^File f (filter good? (filtered-file-seq (io/file dir) visible-files))]
+   (doseq [^File f (filter (all? file? audio?) (visible-file-seq (io/file dir)))]
      (println (.getName f)))))
 
 #_(-main)
