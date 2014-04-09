4decb1b87b1da6012d182c7cafb037e491e3d3ed
Dan Hansen <hansendanr+git@gmail.com>
2014-04-08 20:21:31 -0600

    Upgrade slamhound

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:dependencies [[slamhound "0.5.4"]]}
+  :profiles {:dev {:dependencies [[slamhound "1.5.4"]]}
              :uberjar {:aot :all}}
   :main single-track.core
   :uberjar-name "single-track.jar")
