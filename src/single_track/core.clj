c252a1878dfd94315e99ec75bead24d6494d8ce7
Dan Hansen <hansendanr+git@gmail.com>
2014-04-21 21:29:29 -0600

    Normalize keywords and flatten metadata

--- src/single_track/core.clj
+++ src/single_track/core.clj
@@ -2,10 +2,12 @@
   (:require [clj-logging-config.jul :refer [set-logger-level!]]
             [clojure.java.io :as io]
             [clojure.pprint :refer [pprint]]
-            [clojure.set :refer [rename-keys]])
+            [clojure.set :refer [rename-keys]]
+            [clojure.string :refer [join lower-case]])
   (:import (java.io File)
            (java.util.logging Level)
-           (org.jaudiotagger.audio AudioFileIO))
+           (org.jaudiotagger.audio AudioFileIO)
+           (org.jaudiotagger.tag FieldKey))
   (:gen-class :main true))
 
 (set! *warn-on-reflection* false)
@@ -17,15 +19,32 @@
 (defn audio-header [audiofile]
   (rename-keys (select-keys (bean (.getAudioHeader audiofile))
                             [:trackLength :bitRateAsNumber :format])
-               {:bitRateAsNumber :bitRate}))
+               {:trackLength :track-length :bitRateAsNumber :bit-rate}))
+
+(defn norm-keyword [str]
+  (->> (re-seq #"[A-Z]?[a-z]+" str)
+       (join "-")
+       (lower-case)
+       (keyword)))
+
+(defn convert-longs [m & ks]
+  (apply assoc m (mapcat #(vector % (try (Long/parseLong (m %)) (catch Exception e nil))) ks)))
+
+(def tags
+  [FieldKey/ALBUM FieldKey/ARTIST FieldKey/TITLE FieldKey/TRACK FieldKey/ALBUM_ARTIST FieldKey/GENRE FieldKey/YEAR])
 
 (defn audio-tags [audiofile]
-  (bean (.getTag audiofile)))
+  (when-let [tag (.getTag audiofile)]
+    (convert-longs (into {} (map #(vector (norm-keyword (lower-case (.name %1))) (.getFirst tag %1)) tags))
+                   :year :track)))
 
 (defn metadata [file]
-  (merge {:name (.getName file) :path (.getPath file)}
+  (merge {:name (.getName file)
+          :path (.getPath file)
+          :size (.length file)}
          (when-let [af (audio-file file)]
-           {:header (audio-header af)})))
+           (merge (audio-header af)
+                  (audio-tags af)))))
 
 (defn visible-files [^File file]
   (not= \. (-> file .getName first)))
